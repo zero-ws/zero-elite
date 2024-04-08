4656b2ef053ecf62f5fd908f852328297f0a04ce
silentbalanceyh <silentbalanceyh@126.com>
2024-04-08 15:37:08 +0800

    KDatabase category extend

--- zero-macro/src/main/plugin/io/vertx/up/plugin/database/ZERO.java
+++ zero-macro/src/main/plugin/io/vertx/up/plugin/database/ZERO.java
@@ -8,12 +8,40 @@ import java.util.concurrent.ConcurrentMap;
 
 interface Pool {
 
+    @SuppressWarnings("all")
     ConcurrentMap<EmDS.Category, SQLDialect> DIALECT = new ConcurrentHashMap<>() {
         {
-            this.put(EmDS.Category.ORACLE12, SQLDialect.DEFAULT);
-            this.put(EmDS.Category.MYSQL, SQLDialect.MYSQL);
+            // Jooq Supported Default
+            // MySQL
             this.put(EmDS.Category.MYSQL8, SQLDialect.MYSQL);
             this.put(EmDS.Category.MYSQL5, SQLDialect.MYSQL);
+            this.put(EmDS.Category.TIDB, SQLDialect.MYSQL);
+
+            // PgSQL
+            this.put(EmDS.Category.POSTGRES, SQLDialect.POSTGRES);
+            this.put(EmDS.Category.COCKROACHDB, SQLDialect.POSTGRES);
+
+            // Other
+            this.put(EmDS.Category.MARIADB, SQLDialect.MARIADB);
+            this.put(EmDS.Category.SQLLITE, SQLDialect.SQLITE);
+            this.put(EmDS.Category.TRINO, SQLDialect.TRINO);
+            this.put(EmDS.Category.YUGABYTEDB, SQLDialect.YUGABYTEDB);
+            this.put(EmDS.Category.DERBY, SQLDialect.DERBY);
+            this.put(EmDS.Category.FIREBIRD, SQLDialect.FIREBIRD);
+            this.put(EmDS.Category.H2, SQLDialect.H2);
+            this.put(EmDS.Category.HSQLDB, SQLDialect.HSQLDB);
+
+            // Experimental / Deprecated
+            this.put(EmDS.Category.DUCKDB, SQLDialect.DUCKDB);
+            this.put(EmDS.Category.CUBRID, SQLDialect.CUBRID);
+            this.put(EmDS.Category.IGNITE, SQLDialect.IGNITE);
+
+            // Other will use DEFAULT instead for future
+            for (EmDS.Category category : EmDS.Category.values()) {
+                if (!this.containsKey(category)) {
+                    this.put(category, SQLDialect.DEFAULT);
+                }
+            }
         }
     };
 }
