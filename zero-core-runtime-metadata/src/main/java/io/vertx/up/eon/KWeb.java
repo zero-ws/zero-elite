f726dc8ffd7ce49e4acdbf43be7bb40f18991231
silentbalanceyh <silentbalanceyh@126.com>
2024-04-19 23:17:59 +0800

    Web Scheduler

--- zero-core-runtime-metadata/src/main/java/io/vertx/up/eon/KWeb.java
+++ zero-core-runtime-metadata/src/main/java/io/vertx/up/eon/KWeb.java
@@ -268,4 +268,37 @@ public interface KWeb {
          */
         int MODULE = 10_000_000;
     }
+
+    /**
+     * The Cache Key Definition
+     *
+     * @author <a href="http://www.origin-x.cn">Lang</a>
+     */
+    interface CACHE {
+        String DIRECTORY = "ZERO-CACHE-DIRECTORY";
+
+        String JOB_DIRECTORY = "ZERO-CACHE-JOB_DIRECTORY";
+
+        String REFERENCE = "ZERO-CACHE-REFERENCE";
+
+        String VIEW_FULL = "ZERO-CACHE-VIEW-FULL";
+
+        String RESOURCE = "ZERO-CACHE-RESOURCE";
+
+        String DATABASE = "ZERO-CACHE-DATABASE";
+
+        String DATABASE_MULTI = "ZERO-CACHE_MULTI_DATABASE";
+
+        String UI_LAYOUT = "ZERO-CACHE-TPL";
+
+        interface User {
+            String MY_VIEW = "ZERO-CACHE-USER-VIEW";
+
+            String MY_HABITUS = "ZERO-CACHE-HABITUS";
+
+            String AUTHORIZATION = "ZERO-CACHE-403";
+
+            String AUTHENTICATE = "ZERO-CACHE-401";
+        }
+    }
 }
